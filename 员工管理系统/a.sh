558b77a9013db3d367f4b7384e19928484dd96d7
lsc666 <2294217352@qq.com>
2021-04-10 01:54:23 -0400

    add 员工管理系统

---
+++ 员工管理系统/a.sh
@@ -0,0 +1,2 @@
+gcc server.c -lsqlite3 -o server
+./server 192.168.3.129
---
+++ 员工管理系统/example.c
@@ -0,0 +1,10 @@
+#include "0.h"
+
+
+int main()
+{
+  printf("%d\n",NINE);
+  printf("%d\n",SUCCESS);
+  printf("%d\n",ZERO);
+  return 0;
+}
